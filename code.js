ca2057a9f22574e971e0e94fe5d4581ed390110c
Kenia Valladarez <keniavalladarez@gmail.com>
2016-12-20 07:38:48 -0500

    Completed explorer mode

---
+++ code.js
@@ -0,0 +1,22 @@
+1. Assign the message "Hello, World!" to a variable.
+
+  var greet = 'Hello, World!'
+
+2. Assign a different string to a variable name of your choice.
+
+  var coffee = 'Let\'s go get some coffee'
+
+3. Assign a number to a variable.
+
+  var number = 1
+
+4. Use string concatenation to display the number from #3 in a string.
+
+  var concatenation = '#' + 3
+
+5. Make an array of at least four of your favorite movies or books or bands.
+
+  var favorites = ['Rouge One', 'A Court of Mist and Fury by Sarah J. Maas', 'Harry Potter by J.K Rowling', 'Pierce the Veil']
+
+6. Make a object of information about yourself with at least four properties.
+  var about_me = { firstName: 'Kenia', lastName: 'Arvizu', favoriteThings: favorites, lovesTo: 'Code in Ruby' }
